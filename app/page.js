8f792c4ba2413701be5edfdb87e2f367b54b59e1
bomboonsan <bomboonsan@gmail.com>
2023-08-09 11:49:45 +0700

    update

--- app/page.js
+++ app/page.js
@@ -77,6 +77,7 @@ export default function Home() {
 
   return (
     <>
+    
       <div className='list-cards'>
         {/* <Link href="/game-random"><Card cardThumbnail={'/images/logo.png'} cardTitle={'กรุ๊ปเลือดคุณ เหมาะกับการลงทุนแบบไหนดี?'} cardView={'1234'} /></Link>
         <Link href="/game-fix"><Card cardThumbnail={'/images/logo.png'} cardTitle={'กรุ๊ปเลือดคุณ ?'} cardView={'234'} /></Link>
