270454c8fbf516fa553deeeb9ec78ec6ab4500a8
bomboonsan <bomboonsan@gmail.com>
2023-08-10 15:53:34 +0700

    update logo

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -5,7 +5,17 @@ export default function Header() {
   return (
     <div className='mb-3 p-3'>
         <p className='site-title'>
-            <Link href="/">KTAM</Link>
+            <Link href="/">
+              <Image
+                  className='w-[100px] h-auto block mx-auto'
+                  src="/images/logo-final.png"
+                  alt="KTAM"
+                  // layout="fill"
+                  // objectFit="cover"
+                  width={900}
+                  height={200}
+              />
+            </Link>
         </p>
     </div>
   )
