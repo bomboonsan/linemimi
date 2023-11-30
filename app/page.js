5518f665fd427f41886cbf3689771e334730c394
bomboonsan <83241344+bomboonsan@users.noreply.github.com>
2023-11-30 12:18:07 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -102,7 +102,7 @@ export default function Home() {
             {/* {item._id == '652784aa801b0a0da4fcba03' &&
             <Link href={`/${item.questionType}/${item._id}`}><Card cardThumbnail={`${urlBackend}${item.questionImage}`} cardTitle={item.questionTitle} cardView={item.views} /></Link>
             }     */}
-            {item._id == '655fe901906f48d9dab599d2' && 
+            {item._id == '65681a412970521eb935b881' && 
             <Link href={`/${item.questionType}/${item._id}`}><Card cardThumbnail={`${urlBackend}${item.questionImage}`} cardTitle={item.questionTitle} cardView={item.views} /></Link>
             }   
           </div>
